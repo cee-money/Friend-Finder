6bfe5b6cf5f6c65bac6bf5fd94e17819068e01b8
cmoney <moneymaker.carolyn@gmail.com>
2019-07-08 21:45:44 -0600

    initial file structure, html completion, started server

---
+++ server.js
@@ -0,0 +1,40 @@
+// Dependencies
+var express = require("express");
+var path = require("path");
+
+// Sets up the Express App
+var app = express();
+var PORT = process.env.PORT || 8080;
+
+// Sets up the Express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// Data
+var friends = [
+    {
+      name: "Joe",
+      photo: "https://www.tvovermind.com/wp-content/uploads/2018/05/Joe-Dirt-640x360.jpg",
+      scores: [5, 1, 4, 4, 5, 1, 2, 5, 4, 1]
+    }
+];
+
+// Routes
+app.get("/", function(req, res) {
+    res.sendFile(path.join(__dirname, "app/public/home.html"));
+});
+  
+app.get("/survey", function(req, res) {
+    res.sendFile(path.join(__dirname, "app/public/survey.html"));
+});
+  
+  // Displays all friends
+app.get("/api/friends", function(req, res) {
+    return res.json(friends);
+});
+
+
+// Starts the server to begin listening
+app.listen(PORT, function() {
+    console.log("App listening on PORT " + PORT);
+});
