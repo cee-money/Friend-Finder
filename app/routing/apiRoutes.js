155ab0311a56a7ca29bad394ac3bfc873e8010c0
cmoney <moneymaker.carolyn@gmail.com>
2019-07-10 16:43:27 -0600

    putting routes in route files, debugging logic

---
+++ app/routing/apiRoutes.js
@@ -0,0 +1,96 @@
+var express = require("express");
+
+var router = express.Router();
+
+// Data
+var friends = [
+    {
+      name: "Joe",
+      photo: "https://www.tvovermind.com/wp-content/uploads/2018/05/Joe-Dirt-640x360.jpg",
+      scores: [5, 1, 4, 4, 5, 1, 2, 5, 4, 1]
+    },
+    {
+        name: "Milton",
+        photo: "http://www.jara-jordan.com/wp-content/uploads/2019/04/9-useless-pieces-of-trivia-about-office-space-simple-ideas-design.png",
+        scores: [3, 1, 2, 5, 5, 2, 1, 3, 2, 4]
+    },
+    {
+        name: "Sam",
+        photo: "https://www.girlmuseum.org/wp-content/uploads/2018/06/Molly-Ringwald-Sixteen-Candles-300x205.jpg",
+        scores: [4, 2, 5, 3, 1, 3, 2, 1, 5, 4]
+    }
+];
+
+router.get("/api/friends", function(req, res) {
+    return res.json(friends);
+});
+
+router.post("/api/friends", function(req, res) {
+
+    var newFriend = req.body;
+
+    var scores = newFriend.scores.map(Number);
+
+    newFriend.scores = scores;
+  
+    console.log(newFriend);
+  
+    friends.push(newFriend);
+  
+    res.json(newFriend); 
+});
+
+router.get("/api/match/:name", function(req, res) {
+
+var currentUser = req.params.name;
+
+var currentUserScores = [];
+
+// get current user's scores array
+for (var i = 0; i < friends.length; i++) {
+  if (currentUser === friends[i].name) {
+    currentUserScores = friends[i].scores;
+  }
+}
+
+
+var smallestDiffTracker = {
+    smallestDiffUserIndex: undefined,
+    smallestDiff: undefined
+};
+
+var differences = [];
+
+// compare current user's scores array against all other users' scores arrays
+for (var i = 0; i < friends.length; i++){
+
+    differences = [];
+
+    if (!(currentUser === friends[i].name)) {
+        var compareScores = friends[i].scores;
+
+        for (var j = 0; j < currentUserScores.length; j++) {
+
+            differences.push(Math.abs(currentUserScores[j] - compareScores[j]));
+        }
+        var currentDiff = differences.reduce(function(total, num){
+            return total + num;
+        });
+
+        console.log(`\nFriend: ${friends[i].name}. Sum of difference: ${currentDiff}`)
+
+        if (smallestDiffTracker.smallestDiff === undefined || (currentDiff < smallestDiffTracker.smallestDiff)) {
+
+            smallestDiffTracker.smallestDiffUserIndex = i;
+            smallestDiffTracker.smallestDiff = currentDiff;
+
+        } 
+    } 
+}
+
+res.json(friends[smallestDiffTracker.smallestDiffUserIndex])
+    
+});
+
+// Exporting for server.js
+module.exports = router;
--- server.js
+++ server.js
@@ -1,6 +1,5 @@
 // Dependencies
 var express = require("express");
-var path = require("path");
 
 // Sets up the Express App
 var app = express();
@@ -10,93 +9,12 @@ var PORT = process.env.PORT || 8080;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-// Data
-var friends = [
-    {
-      name: "Joe",
-      photo: "https://www.tvovermind.com/wp-content/uploads/2018/05/Joe-Dirt-640x360.jpg",
-      scores: [5, 1, 4, 4, 5, 1, 2, 5, 4, 1]
-    }
-];
+// Importing routes
+var apiRoutes = require("./app/routing/apiRoutes.js");
+var htmlRoutes = require("./app/routing/htmlRoutes.js");
 
-// Routes
-app.get("/", function(req, res) {
-    res.sendFile(path.join(__dirname, "app/public/home.html"));
-});
-  
-app.get("/survey", function(req, res) {
-    res.sendFile(path.join(__dirname, "app/public/survey.html"));
-});
-  
-// Displays all friends
-app.get("/api/friends", function(req, res) {
-    return res.json(friends);
-});
-
-app.post("/api/friends", function(req, res) {
-
-    var newFriend = req.body;
-
-    var scores = newFriend.scores.map(Number);
-
-    newFriend.scores = scores;
-  
-    console.log(newFriend);
-  
-    friends.push(newFriend);
-  
-    res.json(newFriend); 
-});
-
-app.get("/api/match/:name", function(req, res) {
-
-var currentUser = req.params.name;
-
-var currentUserScores = [];
-
-// get current user's scores array
-for (var i = 0; i < friends.length; i++) {
-  if (currentUser === friends[i].name) {
-    currentUserScores = friends[i].scores;
-  }
-}
-
-
-var smallestDiffTracker = {
-    smallestDiffUserIndex: undefined,
-    smallestDiff: undefined
-};
-
-var differences = [];
-
-// compare current user's scores array against all other users' scores arrays
-for (var i = 0; i < friends.length; i++){
-
-    if (!(currentUser === friends[i].name)) {
-        var compareScores = friends[i].scores;
-
-        for (var j = 0; j < currentUserScores.length; j++) {
-
-            differences.push(Math.abs(currentUserScores[j] - compareScores[j]));
-        }
-        var currentDiff = differences.reduce(function(total, num){
-            return total + num;
-        });
-
-        if (smallestDiffTracker.smallestDiff === undefined || (currentDiff < smallestDiffTracker.smallestDiff)) {
-
-            smallestDiffTracker.smallestDiffUserIndex = i;
-            smallestDiffTracker.smallestDiff = currentDiff;
-
-        } 
-    } 
-}
-
-res.json(friends[smallestDiffTracker.smallestDiffUserIndex])
-    
-});
-
-
+app.use(apiRoutes);
+app.use(htmlRoutes);
 
 // Starts the server to begin listening
 app.listen(PORT, function() {
